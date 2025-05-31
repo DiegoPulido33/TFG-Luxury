79510379d5460977a2e12de3760484e2c57e44b6
Diego Pulido Rivero <puvero22@gmail.com>
2025-05-31 15:58:12 +0200

    formularios confirmados, centrado

--- app/rent/success/page.tsx
+++ app/rent/success/page.tsx
@@ -1,17 +1,20 @@
-import { SiteHeader } from "@/components/site-header"
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { SiteHeader } from "@/components/site-header";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function RentSuccessPage() {
   return (
     <div className="flex min-h-screen flex-col">
       <SiteHeader />
-      <main className="flex-1">
+      <main className="flex flex-1 items-center justify-center">
         <div className="container py-24">
           <div className="mx-auto max-w-2xl text-center">
-            <h1 className="text-4xl font-bold mb-4">!Solicitud de alquiler confirmada!</h1>
+            <h1 className="text-4xl font-bold mb-4">
+              ¡Solicitud de alquiler confirmada!
+            </h1>
             <p className="text-lg text-muted-foreground mb-8">
-             Hemos recibido su solicitud de alquiler. Nuestro equipo se pondrá en contacto con usted cuanto antes, para aclarar los detalles.
+              Hemos recibido su solicitud de alquiler. Nuestro equipo se pondrá
+              en contacto con usted cuanto antes, para aclarar los detalles.
             </p>
             <Button asChild>
               <Link href="/inventory">Ver más vehículos</Link>
@@ -20,5 +23,5 @@ export default function RentSuccessPage() {
         </div>
       </main>
     </div>
-  )
+  );
 }
